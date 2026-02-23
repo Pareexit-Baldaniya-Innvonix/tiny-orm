0e1faffa58ca9447e683d7bd5be6be80e3b378d0
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-23 19:46:11 +0530

    changes done according to comment

--- src/classes/CrudOperations.py
+++ src/classes/CrudOperations.py
@@ -1,6 +1,6 @@
 import mysql.connector
-from .Student import Student
-from .DbConnections import DBConnection
+from classes.Student import Student
+from classes.DbConnections import DBConnection
 
 
 # CRUD operations
@@ -8,20 +8,21 @@ class StudentInfo:
     conn = None
     cursor = None
 
-    @staticmethod
-    def get_cursor():
-        if StudentInfo.conn is None or not StudentInfo.conn.is_connected():
+    @classmethod
+    def get_cursor(cls):
+        if cls.conn is None or not cls.conn.is_connected():
             try:
-                StudentInfo.conn = DBConnection.get_connection()
-                StudentInfo.cursor = StudentInfo.conn.cursor()
+                cls.conn = DBConnection.get_connection()
+                cls.cursor = cls.conn.cursor()
             except mysql.connector.Error as error:
                 print(f"Error connecting to database: {error}")
                 exit(1)
-        return StudentInfo.cursor, StudentInfo.conn
+        return cls.cursor, cls.conn
 
-    @staticmethod
-    def create_table():
-        cursor, _ = StudentInfo.get_cursor()
+    # create students table
+    @classmethod
+    def create_table(cls):
+        cursor, _ = cls.get_cursor()
         try:
             cursor.execute(
                 """
@@ -36,44 +37,49 @@ class StudentInfo:
         except Exception as e:
             print(f"Error: {e}")
 
-    @staticmethod
-    def insert_table(students: list[Student]):
-        cursor, conn = StudentInfo.get_cursor()
+    # add multiple records
+    @classmethod
+    def insert_table(cls, students: list[Student]):
+        cursor, conn = cls.get_cursor()
         sql = "INSERT INTO students(name, email, dept) VALUES (%s, %s, %s)"
         val = [(s.name, s.email, s.dept) for s in students]
         cursor.executemany(sql, val)
         conn.commit()
         print(f"\n--- {cursor.rowcount} data inserted into table. ---")
 
-    @staticmethod
-    def read_table():
-        cursor, conn = StudentInfo.get_cursor()
+    # fetch and print records
+    @classmethod
+    def read_table(cls):
+        cursor, conn = cls.get_cursor()
         cursor.execute("SELECT * FROM students")
         result = cursor.fetchall()
         for row in result:
             print(row)
 
-    @staticmethod
-    def update_table(new_student: Student, old_student: Student):
-        cursor, conn = StudentInfo.get_cursor()
+    # update the data
+    @classmethod
+    def update_table(cls, new_student: Student, old_student: Student):
+        cursor, conn = cls.get_cursor()
         sql = "UPDATE students SET name = %s, email = %s WHERE name = %s AND email = %s"
         val = (new_student.name, new_student.email, old_student.name, old_student.email)
         cursor.execute(sql, val)
         conn.commit()
         print(f"\n--- {cursor.rowcount} row's updated. ---")
 
-    @staticmethod
-    def delete_table(student: Student):
-        cursor, conn = StudentInfo.get_cursor()
+    # delete the data
+    @classmethod
+    def delete_table(cls, student: Student):
+        cursor, conn = cls.get_cursor()
         sql = "DELETE FROM students WHERE name = %s AND email = %s"
         cursor.execute(sql, (student.name, student.email))
         conn.commit()
         print(f"\n--- {cursor.rowcount} data deleted. ---")
 
-    @staticmethod
-    def close_connection():
-        if StudentInfo.cursor:
-            StudentInfo.cursor.close()
-        if StudentInfo.conn and StudentInfo.conn.is_connected():
-            StudentInfo.conn.close()
+    # closing a connection
+    @classmethod
+    def close_connection(cls):
+        if cls.cursor:
+            cls.cursor.close()
+        if cls.conn and cls.conn.is_connected():
+            cls.conn.close()
             print("\n--- Connection closed. ---")
